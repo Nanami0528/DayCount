5ae15384fa1cb0303ee4697f1869b131bb9c2be7
Nanami Tomozoe <nanami@tomozoe.com>
2016-08-22 02:08:41 +0900

    コミ

--- swift_UIDatePickerView_2016/ViewController.swift
+++ swift_UIDatePickerView_2016/ViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var Label: UILabel!
+    
+    // UIDatePicker変更時に呼び出されるメソッド
+    @IBAction func selectDate(sender: UIDatePicker) {
+    
+        // フォーマットを生成
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy/M/dd "
+        // 日付をフォーマットに則って取得.
+        Label.text = dateFormatter.stringFromDate(sender.date)
+        
+        let cal = NSCalendar(identifier: NSCalendarIdentifierGregorian)!
+        let today = NSDate()
+        let targetDate = sender.date
+        var days = cal.components([.Day], fromDate: today, toDate: targetDate, options: NSCalendarOptions.MatchFirst).day
+    
+        //1日ずれるから1足す 
+        days = days + 1
+        print(days)
+     
+           }
+    
+    
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
